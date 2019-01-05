fcb58d2e6a0904ad49c2128ecbf1c3f830b5ee50
Ritesh Dash <riteshdashcfc1236@gmail.com>
2019-01-05 19:33:09 +0530

    Update player.model.js

--- models/player.model.js
+++ models/player.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//the schema used for the Player collection!!!
 const PlaySchema = new Schema({
   player:String,
   age:Number,
