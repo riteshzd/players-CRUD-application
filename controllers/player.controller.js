c8144e6e061bf7557a00c1ae85800a8823c0b44f
Ritesh Dash <riteshdashcfc1236@gmail.com>
2019-01-07 21:02:23 +0530

    Update player.controller.js

--- controllers/player.controller.js
+++ controllers/player.controller.js
@@ -1,28 +1,28 @@
-const Product = require("../models/player.model.js");
+const Player = require("../models/player.model.js");
 
 exports.test = function(req,res){
   res.send("Looks quite good!!!");
 } //the basic testing querry!!!
 
 exports.search = function(req,res){
-  Product.find({position : req.params.pos},function(error,players){
+  Player.find({position : req.params.pos},function(error,players){
     if(error)
     return(error);
-    res.json(players);
+    res.json(players); //the collection is the output!!!
   });
   //searches player as per position!!!
 };
 
 //the function to create a new Player object!!!
 exports.create = function(req,res){
-  let product = new Product({
+  let player = new Player({
     name : req.body.name,
     age : req.body.age,
     club : req.body.club,
     position : req.body.position
   });
 
-  product.save (function(error){
+  player.save (function(error){
     if(error){
       return(error);
     }
@@ -34,7 +34,7 @@ exports.create = function(req,res){
 //the function to read a Player object in json
 exports.read = function(req,res){
   //var idd="5c306ea8617f7d12101711c9";
-  Product.findById(req.params.id,function(error,product){
+  Player.findById(req.params.id,function(error,product){
     if(error){
       return(error);
     }
@@ -46,7 +46,7 @@ exports.read = function(req,res){
 
 //the function to update a Player object!!! 
 exports.update = function(req,res){
-  Product.findByIdAndUpdate(req.params.id,{$set:req.body},function(error){
+  Player.findByIdAndUpdate(req.params.id,{$set:req.body},function(error){
     if(error){
       return(error);
     }
@@ -57,7 +57,7 @@ exports.update = function(req,res){
 
 //the function to delete a Player object!!!
 exports.delete = function(req,res){
-  Product.findByIdAndRemove(req.params.id,function(error){ // player is removed with reference to id
+  Player.findByIdAndRemove(req.params.id,function(error){ // player is removed with reference to id
     res.send("Player removed successfully!!!");
   });
 };
